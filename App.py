e7b0074db301258581cab819285e1f1cb03aa4a6
kriss <kriss.pujats@finabay.com>
2016-11-26 21:22:34 +0200

    init

---
+++ App.py
@@ -0,0 +1,42 @@
+#!/usr/bin/python
+
+import logging
+import sys
+
+from rx import Observable
+
+import Nfc
+import Serial
+from services import FideService, WalmooService
+
+logging.basicConfig(level=logging.INFO, format='%(asctime)s %(message)s')
+
+if len(sys.argv) < 3:
+    raise Exception('params: fideLink walmooLink serialNumber')
+fideLink = sys.argv[1]
+walmooLink = sys.argv[2]
+serialKey = sys.argv[3]
+
+Serial.open()
+
+token = FideService.getToken(fideLink)
+
+cardListener = Observable.create(Nfc.pollingCards).publish()
+
+
+def onCardRead(uid):
+    logging.info(uid)
+    WalmooService.processNewActivity(walmooLink, token, serialKey, uid, sendSerial)
+
+
+def sendSerial(data):
+    logging.info(data)
+    Serial.write(data)
+
+
+cardListener.subscribe(onCardRead)
+try:
+    cardListener.connect()
+except KeyboardInterrupt:
+    logging.info("closing app")
+    Nfc.listeningCards = False
---
+++ services/FideService.py
@@ -0,0 +1,2 @@
+def getToken(link):
+    return 'zGvzIM5MzKFfww5UemfLSjSLUcc_4T8T'
